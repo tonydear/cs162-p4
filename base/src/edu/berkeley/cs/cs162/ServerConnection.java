609aa89423ec10132df855a3784a6eaac8534887
Jason Jee <jj41190@gmail.com>
2011-05-02 19:02:00 -0700

    added blocking queue to serverconnection

--- base/src/edu/berkeley/cs/cs162/ServerConnection.java
+++ base/src/edu/berkeley/cs/cs162/ServerConnection.java
@@ -4,11 +4,15 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public class ServerConnection extends Thread {
 	private ObjectOutputStream oos;
 	private ObjectInputStream ios;
 	private Socket mySocket;
+	private BlockingQueue<TransportObject> toSend;
+	private final static int MAX_SEND = 10000;
 	
 	public ServerConnection(Socket socket){
 		mySocket = socket;
@@ -19,6 +23,7 @@ public class ServerConnection extends Thread {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		toSend = new ArrayBlockingQueue<TransportObject>(MAX_SEND);
 	}
 	
 	public void forwardMessage(TransportObject sendMe){ //send things out
